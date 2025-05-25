9340853f207dc2db7aa3b7edc4038d0b4fee4e9d
Lauren <rg326@live.com>
2025-05-25 17:00:44 -0400

    added topswe and elite19 folders and files 1-fizzbuzz.py and 1_lc121.py

---
+++ topswe/1-fizzbuzz.py
@@ -0,0 +1,21 @@
+#412 - FizzBuzz
+
+# 1st attempt
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        if len(answer) < 1:
+            return answer
+        
+        for i in answer:
+            if answer[i] % 3 == 0: 
+                return "Fizz"
+            elif answer[i] % 5 == 0:
+                return "Buzz"
+            elif answer[i] % 3 == 0 and list[i] % 5 == 0:
+                return "FizzBuzz"
+            else: 
+                answer[i] == i:
+
+
+
+        
